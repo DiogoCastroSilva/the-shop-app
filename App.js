21684bd0ca377baac296272a61141afa5cbd741d
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-05-05 18:02:26 +0100

    Configure products store management

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
+// React
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// Redux
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
+// Reducers
+import producstReducer from './store/reducers/products';
+
+//#region  Data Management
+const rootReducer = combineReducers({
+  prodcuts: producstReducer
+});
+
+const store = createStore(rootReducer);
+//#endregion Data Management
+
+// Component
 export default function App() {
+
+  // View
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      
+    </Provider>
   );
 }
 
+// Styles
 const styles = StyleSheet.create({});
---
+++ screens/shop/Products/Products.js
@@ -0,0 +1,14 @@
+// React
+import React from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+
+// Component
+const Products = () => {
+
+    // View
+    return (
+        <FlatList />
+    );
+};
+
+export default Products;
