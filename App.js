9b8d92ccc21d433649d3a551b6bce4780463ef2c
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-05-06 12:08:14 +0100

    Add fonts to project

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+// React
+import React, { useState } from 'react';
+// Redux
 import { combineReducers, createStore } from 'redux';
 import productReducer from './store/reducers/products';
 import { Provider } from 'react-redux';
-import ShopNavigator from './navigation/ShopNavigator';
+// Expo
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font'
 
+// Navigations
+import ShopNavigator from './navigation/ShopNavigator';
+import { AppLoading } from 'expo';
+
+// Redux config
 const rootReducer = combineReducers({
   products: productReducer
 });
 const store = createStore(rootReducer);
 
+// Fonts
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 export default function App() {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  if (!isFontLoaded) {
+    return <AppLoading
+              startAsync={fetchFonts}
+              onFinish={() => setIsFontLoaded(true)}
+            />
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
